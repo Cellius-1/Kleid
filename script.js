b502f0eed99f19715e2246e37c4a3ec774738384
Tharun Naguleswaran <tharun.nagul@gmail.com>
2024-04-11 22:28:11 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('hide-button').addEventListener('click', function() {
+  var originalImageInput = document.getElementById('original-image');
+  var secretTextInput = document.getElementById('secret-text');
+
+  var originalImageFile = originalImageInput.files[0];
+  var secretText = secretTextInput.value;
+
+  if (!originalImageFile || !secretText) {
+    alert('Please select an image and enter secret text.');
+    return;
+  }
+
+  var formData = new FormData();
+  formData.append('image', originalImageFile);
+  formData.append('secret_text', secretText);
+
+  fetch('/hide', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.blob())
+  .then(blob => {
+    var downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = 'hidden_image.png';
+    downloadLink.click();
+  })
+  .catch(error => console.error('Error:', error));
+});
