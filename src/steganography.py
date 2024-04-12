a8531685c200f35ff992d2862e7b882367cdc517
Tharun Naguleswaran <tharun.nagul@gmail.com>
2024-04-11 22:12:25 -0400

    Update steganography.py

--- src/steganography.py
+++ src/steganography.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from utils import validate_image_file, validate_secret_text
 
 def hide_text_in_image(image_path, secret_text):
     img = Image.open(image_path)
@@ -8,7 +9,7 @@ def hide_text_in_image(image_path, secret_text):
 
     new_img_data = []
     for i in range(secret_text_length):
-        pixel = img_data.__next__()
+        pixel = next(img_data)
         new_pixel = list(pixel)
         new_pixel[-1] = int(binary_secret_text[i])
         new_img_data.append(tuple(new_pixel))
@@ -22,11 +23,21 @@ def reveal_text_in_image(hidden_image_path):
 
     binary_secret_text = ''
     while True:
-        pixel = img_data.__next__()
+        pixel = next(img_data)
         binary_secret_text += str(pixel[-1])
         if len(binary_secret_text) % 8 == 0:
             secret_text = ''.join(chr(int(binary_secret_text[i:i+8], 2)) for i in range(0, len(binary_secret_text), 8))
             return secret_text
 
-hide_text_in_image('original_image.png', 'test text')
-print(reveal_text_in_image('hidden_image.png'))
+if __name__ == "__main__":
+    original_image_path = input("Enter the path of the original image file: ")
+    validate_image_file(original_image_path)
+
+    secret_text = input("Enter the text to hide: ")
+    validate_secret_text(secret_text)
+
+    hide_text_in_image(original_image_path, secret_text)
+    print("Text hidden successfully in the image.")
+
+    hidden_image_path = 'hidden_image.png'
+    print("The hidden image file is saved as:", hidden_image_path)
